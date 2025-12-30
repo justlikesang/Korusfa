4e390ab9a69bfb4465df139c128ff907b398fabc
Sang <sanghosuh@gmail.com>
2025-12-29 16:55:38 -0800

    Remove unused useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Services from './components/Services';
